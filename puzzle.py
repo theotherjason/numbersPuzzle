a38d619d4795bb5b4f4e2fdae026f957551a0099
Toj Fowler <toj@wpengine.com>
2016-07-25 09:50:16 -0500

    Separated puzzleClasses.py / Using puzzle.py as main test
    Also, added to .gitignore

--- puzzle.py
+++ puzzle.py
@@ -1,81 +1,13 @@
 #!/usr/bin/python3
 
-import numpy as np
-
-class puzzleContainer:
-    def __init__(self,x,y,z):
-        self.container = np.zeros((x,y,z))
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def size(self):
-        return self.container.size
-
-    def print(self):
-        #print(self.container)
-        for k in reversed(range(0,self.y)):
-            for j in reversed(range(0, self.y)):
-                for i in range(0, self.x):
-                    # print(self.pieces[num,i,j])
-                    if self.container[i, j, k] == 1:
-                        print("#", end="")
-                    else:
-                        print(" ", end="")
-                print("")
-            print("")
-        print("")
-
-    def add(self,transformedContainerObject):
-        #add two containers
-        #check for any >1 entries & return error if false
-        #otherwise return true
-        return True
-
-
-class puzzlePieces:
-    def __init__(self,num,x,y):
-        self.pieces = np.zeros((num,x,y))
-        self.x = x
-        self.y = y
-
-    def size(self):
-        return self.pieces.size
-
-    def print(self,num):
-        #print(self.pieces[num])
-        for j in reversed(range (0,self.y)):
-            for i in range (0,self.x):
-                #print(self.pieces[num,i,j])
-                if self.pieces[num,i,j] == 1:
-                    print("#", end="")
-                else:
-                    print(" ", end="")
-            print ("")
-        print("")
-
-    def mark(self,num,x,y):
-        self.pieces[num,x,y] = 1
-
-    def clearNumber(self,num):
-        self.pieces[num] = 0
-
-    def piece(self,num):
-        return self.pieces[num]
-
-    def transformedContainer(self,containerSize,num,locationX,locationY,locationZ,orientationX,orientationY,orientationZ):
-        #create empty container
-        #add transformed piece #num to container at locationX,locationY,locationZ
-        #return puzzleContainer with transformed piece
-        return True
-
+import puzzleClasses
 
 def main():
-    numberCase =  puzzleContainer(5,5,5)
+    numberCase =  puzzleClasses.puzzleContainer(5, 5, 5)
     numberCase.print()
     print("The size of the puzzleContainer is",numberCase.size(), "\n")
 
-    numberPieces = puzzlePieces(10,3,5)
+    numberPieces = puzzleClasses.puzzlePieces(10,3,5)
 
     #define 0
     numberPieces.mark(0,0,0)
