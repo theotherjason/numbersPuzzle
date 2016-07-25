9dd778235c87bb3c531b03eab8f9f4393f5c4ff3
Toj Fowler <toj@wpengine.com>
2016-07-25 16:07:12 -0500

    Added commenting in puzzleClasses.py

--- puzzleClasses.py
+++ puzzleClasses.py
@@ -3,6 +3,8 @@
 import numpy as np
 
 class puzzleContainer:
+
+    # The 3d puzzle container will be empty and retain it's x/y/z dimensions
     def __init__(self,x,y,z):
         self.container = np.zeros((x,y,z))
         self.x = x
@@ -10,11 +12,11 @@ class puzzleContainer:
         self.z = z
 
     def size(self):
-
         return self.container.size
 
+    # to print, reverse through z & y and iterate through x
     def print(self):
-        for k in reversed(range(0,self.y)):
+        for k in reversed(range(0,self.z)):
             for j in reversed(range(0, self.y)):
                 for i in range(0, self.x):
                     if self.container[i, j, k] == 1:
@@ -33,6 +35,9 @@ class puzzleContainer:
 
 
 class puzzlePieces:
+
+    # Our actual pieces are in a 3d array of their own, the 1st dimension being each 2d object,
+    # and the other 2 dimensions being the x & y coordinates
     def __init__(self,num,x,y):
         self.pieces = np.zeros((num,x,y))
         self.x = x
@@ -41,6 +46,7 @@ class puzzlePieces:
     def size(self):
         return self.pieces.size
 
+    # To print the array, reverse through y & iterate through x
     def print(self,num):
         for j in reversed(range (0,self.y)):
             for i in range (0,self.x):
@@ -51,9 +57,11 @@ class puzzlePieces:
             print ("")
         print("")
 
+    # Use this to mark each x/y coordinate in each puzzle piece.
     def mark(self,num,x,y):
         self.pieces[num,x,y] = 1
 
+    # Completely clear the number (if needed)
     def clearNumber(self,num):
         self.pieces[num] = 0
 
